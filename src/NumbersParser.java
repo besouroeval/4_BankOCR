24d4fce06b3b458369b825993231d4d73f63445f
somename <someemail>
2010-10-27 15:12:19 -0200

    besouro automatic message

--- src/NumbersParser.java
+++ src/NumbersParser.java
@@ -4,9 +4,9 @@ public class NumbersParser {
 	public static int parse(String number) {
 		if (number.charAt(1) == '_') {
 			if (number.charAt(8) == '|') {
-				return 3;
-			} else {
 				return 2;
+			} else {
+				return 3;
 				
 			}
 		}
