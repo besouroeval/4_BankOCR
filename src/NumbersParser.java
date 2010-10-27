59c56ae40db1d81afc68522edf9058d745527fa3
somename <someemail>
2010-10-27 15:13:44 -0200

    besouro automatic message

--- src/NumbersParser.java
+++ src/NumbersParser.java
@@ -3,7 +3,9 @@ public class NumbersParser {
 
 	public static int parse(String number) {
 		String[] lines = new String[3];
-		lines[0] = number.substring(0,4);
+		lines[0] = number.substring(0,3);
+		lines[1] = number.substring(4,7);
+		lines[2] = number.substring(8,11);
 		if (number.charAt(1) == '_') {
 			if (number.charAt(8) == '|') {
 				return 2;
