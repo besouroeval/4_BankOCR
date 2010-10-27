167243dccccda92a8af25094c83c6a97cc1cf518
somename <someemail>
2010-10-27 15:57:57 -0200

    besouro automatic message

--- src/NumbersParser.java
+++ src/NumbersParser.java
@@ -82,7 +82,7 @@ public class NumbersParser {
 		String[][] lines = new String[qtdDigits][3];
 		
 		for (int i=0 ; i<qtdDigits ; i++) {
-			lines[i][0] = number.substring(0,3);
+			lines[i][0] = number.substring(0+3*i,3+3*i);
 			lines[i][1] = number.substring(4,7);
 			lines[i][2] = number.substring(8,11);
 		}
