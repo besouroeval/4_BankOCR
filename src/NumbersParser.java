357af3d830d947e9a3e3d027937890f9ad897933
somename <someemail>
2010-10-27 15:20:50 -0200

    besouro automatic message

--- src/NumbersParser.java
+++ src/NumbersParser.java
@@ -11,8 +11,10 @@ public class NumbersParser {
 				if (lines[1].charAt(2) == '|') {
 					return 6;
 					
+				} else {
+					return 2;
+					
 				}
-				return 2;
 				
 			} else if (lines[1].charAt(0) == '|') {
 				return 5;
